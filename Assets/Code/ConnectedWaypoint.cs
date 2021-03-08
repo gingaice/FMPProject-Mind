0385cf1dbd42622726eb9b3ab55662654708fa7d
Adam <jones200213@gmail.com>
2021-03-08 12:52:20 +0000

    adding my Ai :)

---
+++ Assets/Code/ConnectedWaypoint.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.Coding
+{
+    public class ConnectedWaypoint : Waypoint
+    {
+        [SerializeField]
+        protected float _connectivityRadius = 50f;
+
+        List<ConnectedWaypoint> _connections;
+
+        public void Start()
+        {
+            //grab all waypoint objects in scene
+            GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+
+            //create a list of waypoints to refer to later
+            _connections = new List<ConnectedWaypoint>();
+
+            //check if they're connected waypoint
+            for (int i = 0; i < allWaypoints.Length; i++)
+            {
+                ConnectedWaypoint nextWaypoint = allWaypoints[i].GetComponent<ConnectedWaypoint>();
+
+                //i.e we found a waypoint
+                if (nextWaypoint != null)
+                {
+                    //at the end of this with the != this checks to see that the next waypoint isnt the one that its on
+                    if (Vector3.Distance(this.transform.position, nextWaypoint.transform.position) <= _connectivityRadius && nextWaypoint != this)
+                    {
+                        _connections.Add(nextWaypoint);
+                    }
+                }
+            }
+        }
+
+        public override void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, debugDrawRadius);
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, _connectivityRadius);
+        }
+        
+        public ConnectedWaypoint nextWaypoint(ConnectedWaypoint previousWaypoint)
+        {
+            if (_connections.Count == 0)
+            {
+                //no waypoints? return null and console complaints
+                Debug.LogError("Insufficient waypoint count :)");
+                return null;
+            }
+            else if (_connections.Count == 1 && _connections.Contains(previousWaypoint))
+            {
+                //Only one waypoint and its the previous one? just use it
+                return previousWaypoint;
+            }
+            else //otherwise find a random one that isnt prevois one
+            {
+                ConnectedWaypoint nextWaypoint;
+                int nextIndex = 0;
+
+                do
+                {
+                    nextIndex = UnityEngine.Random.Range(0, _connections.Count);
+                    nextWaypoint = _connections[nextIndex];
+
+                } while (nextWaypoint == previousWaypoint);
+
+                return nextWaypoint;
+            }
+
+        }
+    }
+}
---
+++ Assets/Code/NPCSimplePatrol.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCSimplePatrol : MonoBehaviour
+{
+    //Dictates whther the agent waits on each node
+    [SerializeField]
+    bool _patrolWaiting;
+
+    //the total time we wait at each node
+    [SerializeField]
+    float _totalWaitTime = 3f;
+
+    //the probability of switching direction
+    [SerializeField]
+    float _switchProbability = 0.2f;
+
+    //the list of all patrol nodes to visit
+    [SerializeField]
+    List<Waypoint> _patrolPoints;
+
+
+
+    //private variables for base behaviour
+    NavMeshAgent _navMeshAgent;
+    int _currentPatrolIndex;
+    bool _travelling;
+    bool _waiting;
+    bool _patrolForward;
+    float _waitTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this is too find the navmesh agent on the object that you put it on
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (_navMeshAgent == null)
+        {
+            Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
+        }
+        else
+        {
+            //this is to find the patrol points that i will be putting into the scene for the ai to go too
+            if(_patrolPoints != null && _patrolPoints.Count >= 2)
+            {
+                _currentPatrolIndex = 0;
+                SetDestination();
+            }
+            else
+            {
+                Debug.Log("insufficient patrol points for basic patrolling behaviour");
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        //checking to see if were close to the destination that we want to go too
+        if(_travelling && _navMeshAgent.remainingDistance <= 1.0f)
+        {
+            _travelling = false;
+
+            //If were going to wait then wait
+            if (_patrolWaiting)
+            {
+                _waiting = true;
+                _waitTimer = 5f;
+            }
+            else
+            {
+                ChangePatrolPoint();
+                SetDestination();
+            }
+        }
+
+        //instead if were waiting
+        if (_waiting)
+        {
+            _waitTimer += Time.deltaTime;
+            if (_waitTimer >= _totalWaitTime)
+            {
+                _waiting = false;
+
+                ChangePatrolPoint();
+                SetDestination();
+            }
+        }
+
+
+    }
+    private void SetDestination()
+    {
+        if (_patrolPoints != null)
+        {
+            Vector3 targetVector = _patrolPoints[_currentPatrolIndex].transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+            _travelling = true;
+        }
+    }
+
+
+    // this selects a new patrol point in the list but also has a small chanc for it to go a different way#
+    private void ChangePatrolPoint()
+    {
+        //this uses unity random featuere to make the npc go either forwards or backwards
+        if (UnityEngine.Random.Range(0f, 1f) <= _switchProbability)
+        {
+            _patrolForward = !_patrolForward;
+        }
+
+        if (_patrolForward)
+        {
+            /**
+            _currentPatrolIndex++;
+
+            if(_currentPatrolIndex >= _patrolPoints.Count)
+            {
+                _currentPatrolIndex = 0;
+            }
+            */
+            //this checks to see how many points there are left and if it has finished the cycle and if it has it resets the cycle
+
+            _currentPatrolIndex = (_currentPatrolIndex + 1) % _patrolPoints.Count;
+        }
+        else
+        {
+            /**
+            _currentPatrolIndex--;
+
+            if(_currentPatrolIndex < 0)
+            {
+                _currentPatrolIndex = _patrolPoints.Count - 1;
+            }
+            */
+
+            if(--_currentPatrolIndex < 0)
+            {
+                _currentPatrolIndex = _patrolPoints.Count - 1;
+            }
+        }
+    }
+}
