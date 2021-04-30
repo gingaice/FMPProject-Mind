0c84352e909f205f525d4b2c1114474c46ab382c
JonesCGD <jones200213@gmail.com>
2021-04-30 14:14:56 +0100

    moving around folders

--- Assets/Shader/Camera/ShaderEffect_CorruptedVram.cs
+++ Assets/Shader/Camera/ShaderEffect_CorruptedVram.cs
@@ -2,11 +2,14 @@
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class ShaderEffect_CorruptedVram : MonoBehaviour {
+public class ShaderEffect_CorruptedVram : MonoBehaviour 
+{
 
-	public float shift = 10;
+	public float shift = 1;
 	private Texture texture;
 	private Material material;
+
+	public float increaser = 1;
 
 	void Awake ()
 	{
@@ -20,4 +23,12 @@ public class ShaderEffect_CorruptedVram : MonoBehaviour {
 		material.SetTexture("_Texture", texture);
 		Graphics.Blit (source, destination, material);
 	}
+
+	void update()
+    {
+		if(TutFOVDetection.checker == true)
+        {
+			shift += Time.deltaTime * increaser;
+        }
+    }
 }
