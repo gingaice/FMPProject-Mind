c8a62066e72ec723e038cfe2fb4586cc9bde36a9
JonesCGD <80253281+gingaice@users.noreply.github.com>
2021-05-06 14:57:05 +0100

    close

--- Assets/Code/Character/CharController.cs
+++ Assets/Code/Character/CharController.cs
@@ -21,6 +21,13 @@ public class CharController : MonoBehaviour
 
     public Slider _stamBar;
 
+    public AnimationClip[] weaponAnimationClip;
+
+    protected Animator animator;
+    protected AnimatorOverrideController animatorOverrideController;
+
+    protected int weaponIndex;
+
     void Start()
     {
         //the camera is facing the way that isometric moves so that the character doesnt move on the z path
@@ -36,6 +43,12 @@ public class CharController : MonoBehaviour
         _stamBar.value = 15;
         _stamBar.wholeNumbers = true;
         _stamBar.gameObject.SetActive(false);
+
+        animator = GetComponent<Animator>();
+        weaponIndex = 0;
+
+        animatorOverrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        animator.runtimeAnimatorController = animatorOverrideController;
     }
 
     // Update is called once per frame
@@ -53,6 +66,7 @@ public class CharController : MonoBehaviour
 
         if (Input.anyKey)
         {
+
             Move();
         }
 
@@ -102,6 +116,8 @@ public class CharController : MonoBehaviour
     {
         if(OutOfSafeSpace.isSafe == false)
         {
+            animatorOverrideController["Sneak Walk"] = weaponAnimationClip[weaponIndex];
+
             Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey"));
             Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
             Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
