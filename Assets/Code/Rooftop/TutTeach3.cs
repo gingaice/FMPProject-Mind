7c595047ce015b502977e114ecb6a826ce3f85df
Adam <jones200213@gmail.com>
2021-05-07 19:32:06 +0100

    rooftop transparency

---
+++ Assets/Code/Rooftop/TutTeach3.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutTeach3 : MonoBehaviour
+{
+    public bool freeze;
+
+    public float _timer = 0;
+    public float _ScreenTime = 0.175f;
+
+    public bool _screenGoUp;
+
+    public Image myPanel;
+    float fadeTime = 3f;
+    Color colorToFadeTo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //myPanel.enabled = false;
+        myPanel.GetComponent<CanvasRenderer>().SetAlpha(0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (freeze == true)
+        {
+            Time.timeScale = 0.1f;
+            _screenGoUp = true;
+        }
+
+        if(_screenGoUp == true)
+        {
+            //myPanel.enabled = true;
+            //myPanel.GetComponent<CanvasRenderer>().SetAlpha(0.5f);
+            myPanel.GetComponent<Image>().CrossFadeAlpha(1.98f, 2.0f, false);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            freeze = true;
+        }
+    }
+}
